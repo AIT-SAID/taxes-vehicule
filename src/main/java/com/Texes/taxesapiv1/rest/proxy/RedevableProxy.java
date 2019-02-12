217d31a7a236c8e0933c22e1c77b058641ce9cb3
saida <saida@saida-PC>
2019-02-12 20:10:07 +0000

    feign

---
+++ src/main/java/com/Texes/taxesapiv1/rest/proxy/RedevableProxy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Texes.taxesapiv1.rest.proxy;
+
+import com.Texes.taxesapiv1.rest.vo.RedevableVo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ *
+ * @author ismail boulaanait
+ */
+@FeignClient(name = "microservice3-taxes", url = "localhost:8099")
+public interface RedevableProxy {
+    @GetMapping("/taxes-api/redevale/cin/{cin}")
+    public RedevableVo findByReference(@PathVariable("cin") String cin);
+}
---
+++ src/main/java/com/Texes/taxesapiv1/rest/vo/RedevableVo.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Texes.taxesapiv1.rest.vo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author YOUNES
+ */
+public class RedevableVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+     private String cin;
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String tel;
+    private String email;
+    private CategorieProduitVo categorieProduitVo;
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+
+    public CategorieProduitVo getCategorieProduitVo() {
+        return categorieProduitVo;
+    }
+
+    public void setCategorieProduitVo(CategorieProduitVo categorieProduitVo) {
+        this.categorieProduitVo = categorieProduitVo;
+    }
+}
